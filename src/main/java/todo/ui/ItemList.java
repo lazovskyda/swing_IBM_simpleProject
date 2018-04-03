de8a2a32c21e48e53f8cd541cd1f627044c50844
lazovskyda <LazovskyDA@qulix.com>
2018-04-03 14:42:27 +0300

    iteamList implementation

---
+++ src/main/java/todo/ui/ItemList.java
@@ -0,0 +1,16 @@
+package todo.ui;
+
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+
+@Component
+public class ItemList extends ArrayList {
+
+
+    public ItemList(){
+        super.add("Item 1112222");
+        super.add("Item 222333");
+        super.add("Item 222333");
+    }
+}
