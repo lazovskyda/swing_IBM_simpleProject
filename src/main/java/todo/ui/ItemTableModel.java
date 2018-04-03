caa25e7cbf147e774c823a8d5aa36a335a971fa1
lazovskyda <LazovskyDA@qulix.com>
2018-04-03 14:37:44 +0300

    iteamTable implemenationa

--- src/main/java/todo/ui/ItemTableModel.java
+++ src/main/java/todo/ui/ItemTableModel.java
@@ -1,11 +1,18 @@
 package todo.ui;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
 import java.util.List;
 
 import javax.swing.table.AbstractTableModel;
 
-public class ItemTableModel extends AbstractTableModel {
 
+@Component
+public class ItemTableModel extends AbstractTableModel {
+	@Autowired
+	@Qualifier("itemList")
 	private List itemList;
 
 	public boolean isCellEditable(int rowIndex, int columnIndex) {
