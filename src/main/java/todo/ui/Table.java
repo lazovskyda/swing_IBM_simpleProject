60ffabfc0e16935a49dd002f2f90ac809c2154e3
lazovskyda <LazovskyDA@qulix.com>
2018-04-03 13:23:18 +0300

    add @Autowired

--- src/main/java/todo/ui/Table.java
+++ src/main/java/todo/ui/Table.java
@@ -1,13 +1,17 @@
 package todo.ui;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 import javax.swing.*;
 import javax.swing.table.TableModel;
 
 @Component("itemTable")
+//@Configuration
 public class Table extends JTable {
+
+
 
     @Autowired
     @Qualifier("itemTableModel")
