5069f45310e9050134b02339785da33c2a88f898
lazovskyda <LazovskyDA@qulix.com>
2018-04-03 13:20:09 +0300

    bean itemTable annotation implemenation1

---
+++ src/main/java/todo/ui/Table.java
@@ -0,0 +1,18 @@
+package todo.ui;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import javax.swing.*;
+import javax.swing.table.TableModel;
+
+@Component("itemTable")
+public class Table extends JTable {
+
+    @Autowired
+    @Qualifier("itemTableModel")
+    @Override
+    public void setModel(TableModel dataModel) {
+        super.setModel(dataModel);
+    }
+}
